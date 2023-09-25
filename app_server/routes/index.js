97b263f8333ddce3e257b449eb69db6c7588dd24
STUDENT Mike P. Collins <Mike.P.Collins@students.ittralee.ie>
2023-09-25 14:43:20 +0100

    Overhauled and added functionality

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const ctrlMain = require('../controllers/main'); 
-/* GET home page. */
-router.get('/', ctrlMain.index); 
+const ctrlLocations = require('../controllers/locations'); 
+const ctrlOthers = require('../controllers/others');
+
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+/* Other pages */
+router.get('/about', ctrlOthers.about);
 module.exports = router;
-
-
