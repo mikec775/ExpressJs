0a925d3c5fd0feef911f41a5cdbca9c5f18d013b
STUDENT Mike P. Collins <Mike.P.Collins@students.ittralee.ie>
2023-10-09 15:16:50 +0100

    Mongo Database Creation

---
+++ app_server/models/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const dbURI = "mongodb+srv://user1:mtu12345@cluster0.00wzfxg.mongodb.net/Loc8r?retryWrites=true&w=majority";
+
+try {
+   
+mongoose.connect(
+    dbURI,
+    { useNewUrlParser: true, useUnifiedTopology: true }).then(
+    () => {console.log(" Mongoose is connected")},
+	err=> {console.log(err)}
+	);
+}
+ catch (e) {
+  console.log("could not connect");
+}
+require('./locations');
+
+
+
